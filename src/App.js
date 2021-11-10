b0425371c340b4ae0ed8c082b1b359d7080f8985
Aron Kuch <AronKuch@gmail.com>
2021-11-10 08:03:41 -0500

    Add icons and TailwindsCSS.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import {BsCalendarEvent as CalIcon}  from "react-icons/bs";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Appointments</h1>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl">
+        <CalIcon className="inline-block text-blue-600 align-top" /> Appointments</h1>
     </div>
   );
 }
