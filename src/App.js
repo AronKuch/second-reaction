d520951b7fe2575931ab4a8554d27133fc15eb44
Aron Kuch <AronKuch@gmail.com>
2021-11-12 19:17:19 -0500

    Changed data import to use useEffect and useCallback

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import   {useState, useEffect, useCallback} from 'react';
 import {BsCalendarEvent as CalIcon}  from "react-icons/bs";
 import Search from "./components/Search";
 import AddAppointment from "./components/AddAppointment";
-import appointmentList from "./data.json";
 import AppointmentInfo from "./components/AppointmentInfo";
 
 function App() {
+
+  let [appointmentList, setAppointmentList] = useState([]);
+
+  const fetchData = useCallback(() => {
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(data => {
+        setAppointmentList(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetchData()
+  }, [fetchData]);
+
+
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl mb-5">
